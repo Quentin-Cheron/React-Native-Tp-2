306e35282e125164ad7ec47bd9daedb69d40cc73
Quentin-Cheron <quentin.cheron26200@hotmail.com>
2024-06-04 17:21:15 +0200

    add readme

--- app/(restaurant)/restaurant.js
+++ app/(restaurant)/restaurant.js
@@ -1,6 +1,7 @@
-import { FlatList, StyleSheet } from "react-native";
-import { Text, View, Button } from "react-native";
-import { Image } from "react-native-elements";
+import { FlatList, ScrollView, StyleSheet } from "react-native";
+import { Text, View } from "react-native";
+import { Image } from "expo-image";
+import { Button } from "react-native-elements";
 
 export default function RestaurantScreen() {
   const restaurant = {
@@ -15,51 +16,63 @@ export default function RestaurantScreen() {
         price: "10€",
         description: "Description du produit 1",
         favoris: "1300",
-        image: "https://via.placeholder.com/150",
+        image:
+          "https://www.ridesharingforum.com/uploads/default/original/2X/5/5aac259337e048fdca663a0418f40d1366bc4cda.jpg",
       },
       {
         name: "Produit 2",
         price: "15€",
         favoris: "1500",
         description: "Description du produit 2",
-        image: "https://via.placeholder.com/150",
+        image:
+          "https://www.ridesharingforum.com/uploads/default/original/2X/5/5aac259337e048fdca663a0418f40d1366bc4cda.jpg",
       },
       {
         name: "Produit 3",
         price: "20€",
         favoris: "2000",
         description: "Description du produit 3",
-        image: "https://via.placeholder.com/150",
+        image:
+          "https://www.ridesharingforum.com/uploads/default/original/2X/5/5aac259337e048fdca663a0418f40d1366bc4cda.jpg",
       },
     ],
   };
 
   return (
-    <View>
-      <View style={styles.containerHeader}>
-        <Text style={styles.title}>{restaurant.title}</Text>
-        <Text style={styles.description}>{restaurant.address}</Text>
-        <Text style={styles.address}>{restaurant.description}</Text>
-      </View>
-      <View>
-        <Text>{restaurant.Fee}</Text>
-        <Text>{restaurant.time}</Text>
-      </View>
-      <Text>Produits:</Text>
+    <>
+      <ScrollView>
+        <View>
+          <View style={styles.containerHeader}>
+            <Text style={styles.title}>{restaurant.title}</Text>
+            <Text style={styles.description}>{restaurant.address}</Text>
+            <Text style={styles.address}>{restaurant.description}</Text>
+          </View>
+          <View style={styles.info}>
+            <Text>Frais: {restaurant.Fee}</Text>
+            <Text>Temps: {restaurant.time}</Text>
+          </View>
+          <Text style={styles.titleProduit}>Produits:</Text>
+        </View>
+      </ScrollView>
       <FlatList
         data={restaurant.products}
         renderItem={({ item }) => (
-          <View>
+          <View style={styles.product}>
             <Text>{item.name}</Text>
             <Text>{item.price}</Text>
             <Text>{item.description}</Text>
             <Text>{item.favoris}</Text>
-            <Image source={{ uri: item.image }} />
+            <Image
+              source={{ uri: item.image }}
+              style={styles.image}
+              contentFit="contain"
+            />
+            <Button title="Ajouter au panier" style={styles.button} />
           </View>
         )}
         keyExtractor={(item) => item.name}
       />
-    </View>
+    </>
   );
 }
 
@@ -77,5 +90,32 @@ const styles = StyleSheet.create({
   },
   address: {
     textAlign: "center",
+  },
+  product: {
+    padding: 10,
+    borderWidth: 1,
+    borderColor: "#ccc",
+    marginBottom: 20,
+    marginHorizontal: 20,
+  },
+  info: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+    borderWidth: 1,
+    borderColor: "#ccc",
+    padding: 10,
+    margin: 20,
+  },
+  titleProduit: {
+    margin: 20,
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+  image: {
+    width: "100%",
+    height: 200,
+  },
+  button: {
+    margin: 10,
   },
 });
---
+++ readme.md
@@ -0,0 +1,59 @@
+# Application de commande de produit en ligne
+
+## Description
+
+Ce projet est une application de commande de produit en ligne. Il permet à un utilisateur de commander un produit en ligne. L'application est composée de deux parties : une partie client et une partie administrateur. La partie client permet à un utilisateur de commander un produit en ligne. La partie administrateur permet à un administrateur de gérer les produits.
+
+## Fonctionnalités
+
+### Partie client
+
+- Afficher la liste des restaurants
+
+- Afficher les détails d'un restaurant
+
+- Commander un produit
+
+### Partie administrateur
+
+- Ajouter un produit
+
+- Modifier un produit
+
+- Supprimer un produit
+
+### Authentification
+
+- Authentification du client
+
+## Technologies utilisées
+
+- React-Native
+
+- Supabase
+
+- Expo
+
+## Installation
+
+1. Cloner le dépôt
+
+```bash
+https://github.com/Quentin-Cheron/React-Native-Tp-2.git
+```
+
+2. Installer les dépendances
+
+```bash
+npx expo install
+```
+
+3. Lancer l'application
+
+```bash
+npx expo start
+```
+
+## Temps de réalisation
+
+Le temps de réalisation de ce projet est de 2 jours.
